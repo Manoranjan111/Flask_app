424f54d09f2157360cdd9d412d2efbf50ad1b86f
MANORANJAN <95931051+Manoranjan111@users.noreply.github.com>
2023-02-22 18:21:54 +0000

    added contact, about, project

--- main.py
+++ main.py
@@ -8,6 +8,14 @@ def hello_world():
   return render_template('home.html')
 
 
+@app.route("/about")
+def about_form():
+  return render_template('about.html')
+
+@app.route("/project")
+def project_form():
+  return render_template('project.html')
+
 @app.route("/contact")
 def contact_form():
   return render_template('contact.html')
